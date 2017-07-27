b177b0a66edbe8fe9d2a5398f684c1b9a54c74cd
oskarromeroASC4 <romeroo@bxscience.edu>
2017-07-27 09:39:39 -0400

    Week 1 and 2 Projects

--- week1/p5/Oskar-spaceinvaders/spaceinvaders.js
+++ week1/p5/Oskar-spaceinvaders/spaceinvaders.js
@@ -22,21 +22,32 @@ var y = 10;
 var speed = 5;
 
 function setup(){
-    createCanvas(600, 600);
+    createCanvas(600, 700);
     background("grey");
 }
 
 function draw(){
     background("grey");
-    fill('green');
-    rect(((width/2) - 50) + x, 550, 100, 30);
-    rect(((width/2) - 15) + x, 530, 30, 20);
+    drawShip();
     move();
     drawAlien();
     addshot();
     disappear();
+    drawScore();
+    drawLives();
     }
     
+ function drawShip(){
+    fill('green');
+ var shipXpos = (((width/2) - 50) + x); 
+    rect(shipXpos, 550, 100, 30);
+    rect(((width/2) - 15) + x, 530, 30, 20);
+        if (shipXpos >= 600 ){
+            x = -x + 100 ;
+        }else if(shipXpos <= 0){
+            x = -x;
+        }
+ }
 function move(){
 if (keyIsDown(LEFT_ARROW)){
 x -= 5;
@@ -58,19 +69,30 @@ function addshot(){
         shots[i].ypos = shots[i].ypos - y;
     }
 }
+
 var xspeed = 2;
-var yspeed = 0.2;
+var yspeed = 10;
 
 function drawAlien(){
-    for (var i =0; i <= alien.length-1; i++){
+    for (var i =0; i < alien.length; i++){
         fill("green");
-        rect(alien[i].xpos,alien[i].ypos + 50,alien[i].width,alien[i].height);
-        alien[i].ypos = alien[i].ypos + yspeed
-        alien[i].xpos = alien[i].xpos + xspeed
-        if(alien[i].xpos < 0 || alien[i].xpos > 100 + (i * 90)){
+        rect(alien[i].xpos,alien[i].ypos,alien[i].width,alien[i].height);
+        alien[i].xpos = alien[i].xpos + xspeed;
+        
+        if(alien[i].xpos < 0 || alien[i].xpos > 600 - (alien[i].width) ){
             xspeed = -xspeed;
+            for (var j =0; j < alien.length; j++){
+                alien[j].ypos = alien[j].ypos + yspeed;
             }
-}
+        }
+       if(alien[i].ypos >= 510){
+           background("black");
+           fill("white");
+           textSize(50);
+           textAlign(CENTER);
+           text("YOU LOSE", 305,260);
+       }
+    }
 }
 function disappear(){
     for (var i = 0; i < alien.length;i++){
@@ -78,10 +100,58 @@ function disappear(){
         if (shots[j].xpos >= alien[i].xpos && shots[j].xpos <= alien[i].xpos 
         + alien[i].width && shots[j].ypos >= alien[i].ypos && shots[j].ypos 
         <= alien[i].ypos + alien[i].height) {
+        shots.splice(j,j);
          alien.splice(i,1);
-         shots.splice(j,1);
+         score = score + 10;
+         
         }
         
         }
     }
 }
+
+var score = 0;
+function drawScore(){
+    textFont("Georgia");
+    fill('black');
+    textSize(25);
+    text("Score: " + score, 20, 500, 100, 100);
+}
+
+function Ships(xpos, ypos, width, height){
+    this.xpos = xpos
+    this.ypos = ypos
+    this.width = width
+    this.height = height
+}
+
+function Shooter(xpos, ypos, width, height){
+    this.xpos = xpos
+    this.ypos = ypos
+    this.width = width
+    this.height = height
+}
+
+var ships = [];
+for(i = 0; i < 4; i++){
+    ships.push(new Ships(350 + (i * 60), 655, 50, 15));
+}
+
+var shooters = [];
+for(i = 0; i < 4; i++){
+    shooters.push(new Shooter(368 + (i * 60), 645, 15, 10));
+}
+
+function drawLives(){
+    fill('black');
+    textSize(20);
+    text('Lives: ', 280, 650, 100, 100);
+    for (i = 0; i < ships.length; i++){
+        fill("green");
+        rect(ships[i].xpos,ships[i].ypos,ships[i].width,ships[i].height);
+    }
+    for (i = 0; i < shooters.length; i++){
+        fill("green");
+        rect(shooters[i].xpos,shooters[i].ypos,shooters[i].width,shooters[i].height);
+}
+}   
