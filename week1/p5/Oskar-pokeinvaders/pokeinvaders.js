7eab16bf9360647c58b5f5112a573fb1b334c22e
oskarromeroASC4 <romeroo@bxscience.edu>
2017-08-17 09:55:36 -0400

    Space Invaders

--- week1/p5/Oskar-pokeinvaders/pokeinvaders.js
+++ week1/p5/Oskar-pokeinvaders/pokeinvaders.js
@@ -1,175 +1,240 @@
 
 
-function Aliens( xpos,ypos,width,height){
-this.xpos = xpos;
-this.ypos = ypos;
-this.width = width;
-this.height = height;
 
+function Aliens(xpos, ypos, width, height) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.width = width;
+    this.height = height;
 
 }
+
+
 
 var alien = [];
-var alien2 = [];
-var alien3 = [];
-var alien4 = [];
 
-for (var i =0; i<10; i++){
-    for(var j = 0; j <4; j++){
-    alien.push(new Aliens(30 + (i*90),10 + (j*80),70,70));
+for (var i = 0; i < 13; i++) {
+    for (var j = 0; j < 5; j++) {
+        alien.push(new Aliens(30 + (i * 90), 10 + (j * 80), 70, 70));
     }
 }
 
 
-
-
-
-
-
-function Shot(xpos,ypos,width,height){
-this.xpos = xpos;
-this.ypos = ypos;
-this.width = width;
-this.height = height;
+function Shot(xpos, ypos, width, height) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.width = width;
+    this.height = height;
 }
 var shots = [];
+
+function Bullet(xpos, ypos, width, height) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.width = width;
+    this.height = height;
+}
+var bullet = [];
 var a = 0;
-var x =0;
-var y = 10;
-
-
-function setup(){
-    createCanvas(1000, 700);
-    
-    ballImg = loadImage("https://vignette2.wikia.nocookie.net/ashsencounters/images/6/62/Ash_holding_a_Pokeball_transparent.gif/revision/latest?cb=20140701175439");
-    pokeImg = loadImage("https://pbs.twimg.com/profile_images/736489088/pokebola.gif");
-    pikaImg = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+Math.floor(Math.random()*700)+".png");
-    bulbImg = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png");
-    charImg = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png");
-    mewImg = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png");
-    lifeImg = loadImage("https://vignette2.wikia.nocookie.net/shipping/images/6/66/Pixel_heart_icon.png/revision/latest?cb=20151011174450");
-}
-
-function draw(){
-    background("grey");
-    drawShip();
-    move();
-    drawAlien();
-    addshot();
-    disappear();
-    drawScore();
-    drawLives();
+var x = 0;
+var y = 5;
+var cool = 'true';
+function keyPressed() {
+    if (keyCode == 32) {
+        shots.push(new Shot(barco.xpos + 420, 730, 45, 30));
     }
-    
- function drawShip(){
-    fill('green');
- var shipXpos = (((width/2) - 50) + x); 
-    image(ballImg, shipXpos, 550, 100, 50);
- }
-
-function move(){
-if (keyIsDown(LEFT_ARROW)){
-x -= 5;
 }
-if (keyIsDown(RIGHT_ARROW)){
-x += 5;
-}
-}
-
-function keyPressed(){
-    if (keyCode == 32){
-             shots.push(new Shot((((width/2) - 15) + x), 530, 45, 30));
-        }
-    }
-function addshot(){
-    for(var i = 0; i < shots.length; i++){
+function addshot() {
+    for (var i = 0; i < shots.length; i++) {
         fill("white");
         image(pokeImg, shots[i].xpos, shots[i].ypos, shots[i].width, shots[i].height);
         shots[i].ypos = shots[i].ypos - y;
     }
 }
+function addbullet() {
+    for (var i = 0; i < bullet.length; i++) {
+        fill("white");
+        ellipse(bullet[i].xpos, bullet[i].ypos, bullet[i].width, bullet[i].height);
+        bullet[i].ypos = bullet[i].ypos + y;
+    }
+}
+
+function Attack(xpos, ypos, width, height) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.width = width;
+    this.height = height;
+}
+
+
+
+// To set up the canvas
+function setup() {
+    createCanvas(1350, 780);
+    nice = loadImage("https://media.giphy.com/media/7cTJ3gWVsoC08/giphy.gif");
+    ballImg = loadImage("https://vignette2.wikia.nocookie.net/ashsencounters/images/6/62/Ash_holding_a_Pokeball_transparent.gif/revision/latest?cb=20140701175439");
+    pokeImg = loadImage("https://pbs.twimg.com/profile_images/736489088/pokebola.gif");
+    pikaImg = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + Math.floor(Math.random() * 700) + ".png");
+    bulbImg = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png");
+    charImg = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png");
+    mewImg = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png");
+    lifeImg = loadImage("https://vignette2.wikia.nocookie.net/shipping/images/6/66/Pixel_heart_icon.png/revision/latest?cb=20151011174450");
+
+}
+
+function draw() {
+    background(nice);
+    drawShip();
+    move();
+    drawAlien();
+    addshot();
+    addbullet();
+    disappear();
+    drawScore();
+    drawLives();
+    shootBack();
+    loseLife();
+
+
+}
+
+// To draw the main ship 
+function mainShip(xpos, ypos, width, height) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.width = width;
+    this.height = height;
+}
+
+var barco = new mainShip(250, 550, 100, 50);
+
+function drawShip() {
+    fill('green');
+    var shipXpos = (((windowWidth / 2) - 250) + x);
+    image(ballImg, barco.xpos + 400, 730, 100, 50);
+}
+
+function move() {
+    if (keyIsDown(LEFT_ARROW)) {
+        barco.xpos = barco.xpos - 5;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        barco.xpos = barco.xpos + 5;
+    }
+}
+
+
 
 var xspeed = 1.5;
 var yspeed = 10;
 
-function drawAlien(){
-    for (var i =0; i < alien.length; i++){
-        image(pikaImg, alien[i].xpos,alien[i].ypos,alien[i].width +50 ,alien[i].height+50);
+function drawAlien() {
+    for (var i = 0; i < alien.length; i++) {
+        image(pikaImg, alien[i].xpos, alien[i].ypos, alien[i].width + 50, alien[i].height + 50);
         alien[i].xpos = alien[i].xpos + xspeed;
-        
-        if(alien[i].xpos < 0 || alien[i].xpos > 1000 - (alien[i].width) ){
-            xspeed = -xspeed;            
-            for (var j =0; j < alien.length; j++){
+
+        if (alien[i].xpos < 0 || alien[i].xpos > 1350 - (alien[i].width)) {
+            xspeed = -xspeed;
+            for (var j = 0; j < alien.length; j++) {
                 alien[j].ypos = alien[j].ypos + yspeed;
             }
         }
-       if(alien[i].ypos >= 510){
-           background("black");
-           fill("white");
-           textSize(50);
-           textAlign(CENTER);
-           text("YOU LOSE", 500,260);
-       }
-    }
-
-}
-function disappear(){
-    for (var i = 0; i < alien.length;i++){
-        for(var j = 0; j < shots.length; j++){
-        if (shots[j].xpos >= alien[i].xpos && shots[j].xpos <= alien[i].xpos 
-        + alien[i].width && shots[j].ypos >= alien[i].ypos && shots[j].ypos 
-        <= alien[i].ypos + alien[i].height) {
-        shots.splice(j,j);
-         alien.splice(i,1);
-         score = score + 10;
-         
-        }
-        
+        if (alien[i].ypos >= 510) {
+            background("black");
+            fill("white");
+            textSize(50);
+            textAlign(CENTER);
+            text("YOU LOSE", 500, 260);
         }
     }
 
+}
+function disappear() {
+    for (var i = 0; i < alien.length; i++) {
+        for (var j = 0; j < shots.length; j++) {
+            if (shots[j].xpos >= alien[i].xpos && shots[j].xpos <= alien[i].xpos
+                + alien[i].width && shots[j].ypos >= alien[i].ypos && shots[j].ypos
+                <= alien[i].ypos + alien[i].height) {
+                shots.splice(j, j);
+                alien.splice(i, 1);
+                score = score + 10;
+
+            }
+
+        }
+    }
 
 }
+
+function loseLife() {
+    for (var i = 0; i < ships.length; i++) {
+        for (var j = 0; j < bullet.length; j++) {
+            if (bullet[j].xpos >= (barco.xpos + 400) && bullet[j].xpos <= (barco.xpos + 400
+                + barco.width) && bullet[j].ypos >= barco.ypos && bullet[j].ypos
+                <= barco.ypos + barco.height) {
+                bullet.splice(j, j);
+                ships.splice(i, 1);
+            }
+
+        }
+
+    }
+    ``
+}
+
+
+
 
 var score = 0;
-function drawScore(){
+
+function drawScore() {
     textFont("Georgia");
     fill('black');
     textSize(25);
     text("Score: " + score, 20, 500, 100, 100);
 }
 
-function Ships(xpos, ypos, width, height){
-    this.xpos = xpos
-    this.ypos = ypos
-    this.width = width
-    this.height = height
+function Ships(xpos, ypos, width, height) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.width = width;
+    this.height = height;
 }
 
-function Shooter(xpos, ypos, width, height){
-    this.xpos = xpos
-    this.ypos = ypos
-    this.width = width
-    this.height = height
-}
 
 var ships = [];
-for(i = 0; i < 4; i++){
+for (i = 0; i < 4; i++) {
     ships.push(new Ships(350 + (i * 60), 655, 50, 15));
 }
 
-var shooters = [];
-for(i = 0; i < 4; i++){
-    shooters.push(new Shooter(368 + (i * 60), 645, 15, 10));
-}
 
-function drawLives(){
-    fill('black');
-    textSize(20);
-    text('Lives: ', 710, 655, 100, 100);
-    for (i = 0; i < 3; i++){
-        fill("green");
-        image(lifeImg, ships[i].xpos + 450,ships[i].ypos,ships[i].width - 10,ships[i].height+10);
+
+function drawLives() {
+    if (cool == 'true') {
+        fill('white');
+        textSize(30);
+        text('Lives: ', 1020, 722, 100, 100);
+        for (i = 0; i < 3; i++) {
+            fill("green");
+            image(pikaImg, ships[i].xpos + 800, ships[i].ypos + 50, ships[i].width + 40, ships[i].height + 60);
+        }
     }
     
-}   
+}
+window.setTimeout(shootBack, 4000);
+var x = Math.random();
+var y = Math.floor(Math.random() * (alien.length - 1))
+function shootBack() {
+    if (x < 0.03) {
+        bullet.push(new Bullet(alien[y].xpos, alien[y].ypos, 20, 20));
+        alien[y].ypos += 0.00001;
+        x = Math.random();
+        y = Math.floor(Math.random() * (alien.length - 1))
+
+    } else {
+        x = Math.random();
+        y = Math.floor(Math.random() * (alien.length - 1))
+    }
+    x = Math.random();
+    y = Math.floor(Math.random() * (alien.length - 1))
+}
---
+++ week1/p5/Oskar-pokeinvaders/scoreboard.js
@@ -0,0 +1,276 @@
+var init = function(){
+   //~~~~~~~~~~~~~~~~~~~~~~~~//
+    // this is all that is needed to run the module!
+    var options={
+    // customize the following:
+    // syntax is like css, but make sure:
+    // key and value are both strings inside each option!
+    "scoreboardWidth":"90%", //recommended
+    "scoreboardHeight":"400", // recommended
+    "submitDialogWidth":"auto", //recommended
+    "submitDialogHeight":"auto", // recommended
+    burey:{
+// Scoreboard Module By Burey text
+    "font-family":"Times New Roman",
+    "font-style":"italic",
+    "text-shadow": "5px 2px 4px grey",
+    "color": "green",
+    "background-color":"white",
+    "border-radius": "25px"
+    },
+    dialogTitle:{
+    // dialogs title bar
+    "background":"black",
+    "color":"white"
+    },
+    scoreboardContainer:{
+    // scoreboard container dialog
+    "background-color":"#97B5F5"
+    },
+    tableHeader:{
+    // # Name Score Time
+    "font-family":"Times New Roman",
+    "color": "black"
+    },
+    scorePosition:{
+    // #
+    "font-family":"Times New Roman",
+    "color": "blue"
+    },
+    scoreName:{
+    // name of user
+    "font-family":"'Press Start 2P', Times New Roman",
+    "font-size":"10px",
+    "color": "red",
+    "word-wrap": "break-word",
+    "max-width":"110px"
+    },
+    scoreValue:{
+    // score value
+    "font-family":"Times New Roman",
+    "font-size": "15px",
+    "color": "green"
+    },
+    scoreTime:{
+    // time + date
+    "font-family":"Times New Roman",
+    "font-size":"12px",
+    "color": "yellow"
+    },
+    newScoreContainer:{
+    // submit new score dialog
+    "background-color":"#97B5F5"
+    },
+    scoreYourScoreLabel:{
+    // 'Your Score:' text in new score dialog
+    "font-family":"Times New Roman",
+    "color": "green"
+    },
+    scoreValueLabel:{
+    // score value in new score dialog
+    "font-family":"Times New Roman",
+    "color": "blue",
+    "margin-right":"25px"
+    },
+    scoreErrorLabel:{
+    // error label in new score dialog
+    "font-family":"Times New Roman",
+    "color": "red"
+    },
+    scoreboardButtons:{
+    // scoreboard dialog buttons
+    "font-family":"Times New Roman",
+    "text-shadow": "5px 2px 4px grey",
+    "background":"red",
+    "color": "white",
+    "display":"block" // use "none" to hide button
+    },
+    newScoreButtons:{
+    // new score dialog buttons
+    "font-family":"Times New Roman",
+    "text-shadow": "5px 2px 4px grey",
+    "background":"black",
+    "color": "white"
+    },
+    dialogButtonPanels:{
+    "font-family":"Times New Roman",
+    "text-shadow": "5px 2px 4px grey",
+    "background":"#97B5F5",
+    "color": "red"
+    },
+    sortDropDownList:{
+    "font-family":"'Press Start 2P', Times New Roman",
+    "font-size":"20px",
+    "margin-left":"5px"
+    },
+    loaderOptions:{
+    // scoreboard loading animation
+    "border-bottom": "16px solid #888",
+    "border-top": "16px solid #888",
+    "border-right": "16px solid green",
+    "border-left": "16px solid green",
+    "margin-left": "auto",
+    "margin-right": "auto"
+    }
+};
+var options2={
+    // customize the following:
+    // syntax is like css, but make sure:
+    // key and value are both strings!
+    "scoreboardWidth":"90%", //recommended
+    "scoreboardHeight":"400", // recommended
+    "submitDialogWidth":"auto", //recommended
+    "submitDialogHeight":"auto", // recommended
+         burey:{
+    // Scoreboard Module By Burey text
+         "font-family":"Times New Roman",
+         "font-style":"italic",
+         "text-align":"center",
+         "box-shadow":"0px 0px 3px #7710ff, inset 0px 0px 5px 1px black",
+         "text-shadow": "5px 2px 4px grey",
+         "color": "#9910ff",
+         "background-color":"white",
+         "border-radius": "25px"
+         },
+         dialogTitle:{
+         // dialogs title bar
+         "box-shadow":"0px 0px 3px #7710ff, inset 0px 0px 5px 1px white",
+         "text-shadow": "0px 0px 1px #e0d0ff",
+         'border':'1px solid #7710bb',
+         "border-radius":"2%",
+         "font-family":"Times New Roman",
+         "text-align":"center",
+         "background":"black",
+         "color":"#9910ff"
+         },
+         scoreboardContainer:{
+    // scoreboard container dialog
+        "border":"1px solid grey",
+         //"border-radius":"1%",
+         "box-shadow":"0px 0px 3px #7710ff, inset 0px 0px 5px 1px white",
+         "background-color":"black"
+         },
+         tableHeader:{
+    // # Name Score Time
+         "border-radius":"10px",
+         "font-family":"Times New Roman",
+         "text-align":"center",
+         "border":"1px solid #7710bb",
+"box-shadow":"0px 0px 2px #9910ff, inset 0px 0px 3px white",
+"text-shadow": "0px 0px 1px #e0d0ff",
+         "color": "#9910ff"
+         },
+         scorePosition:{
+    // #
+         "font-family":"Times New Roman",
+         "color": "#9910ff"
+         },
+         scoreName:{
+    // name of user
+         "font-family":"Times New Roman",
+         "color": "#9910ff",
+         "word-wrap": "break-word",
+         "max-width":"110px"
+         },
+         scoreValue:{
+    // score value
+         "font-family":"Times New Roman",
+         "font-size": "15px",
+         "color": "#9910ff"
+         },
+         scoreTime:{
+    // time + date
+         "font-family":"Times New Roman",
+         "font-size":"12px",
+         "color": "#9910ff"
+         },
+         newScoreContainer:{
+    // submit new score dialog
+         "background-color":"black"
+         },
+         scoreYourScoreLabel:{
+    // 'Your Score:' text in new score dialog
+         "font-family":"Times New Roman",
+         "color": "#9910ff"
+         },
+         scoreValueLabel:{
+    // score value in new score dialog
+         "font-family":"Times New Roman",
+         "color": "#9910ff",
+         "margin-right":"25px"
+         },
+         scoreErrorLabel:{
+    // error label in new score dialog
+         "font-family":"Times New Roman",
+         "color": "red"
+         },
+         scoreboardButtons:{
+    // scoreboard dialog buttons
+         "font-family":"Times New Roman",
+         "text-shadow": "5px 2px 4px grey",
+         "background":"black",
+         "color": "#9910ff",
+         "display":"block" // remove button
+         },
+         newScoreButtons:{
+    // new score dialog buttons
+        "font-family":"Times New Roman",
+         "text-shadow": "5px 2px 4px grey",
+         "background":"black",
+         "color": "#9910ff"
+         },
+         dialogButtonPanels:{
+         "font-family":"Times New Roman",
+         "text-shadow": "5px 2px 4px grey",
+         "background":"black",
+         "color": "#9910ff"
+         },
+         sortDropDownList:{
+            "font-family":"Times New Roman",
+            "font-size":"20px",
+            "margin-left":"5px"
+         },
+         loaderOptions:{
+     // scoreboard loading animation
+        "border-bottom": "25px solid #888", /* Light grey */
+        "border-top": "25px solid #888", /* Light grey */
+        "border-right": "25px solid #9910ff",
+        "border-left": "25px solid #9910ff",
+        "margin-left": "auto",
+        "margin-right": "auto"
+    }
+};
+
+    var scoreboard = null;
+   $(document).ready(function() {
+       // load the scoreboard only when done loading the script!!!
+      scoreboard = new Scoreboard(options2);
+   });
+    
+    
+   
+   
+ /// USAGE EXAMPLE WITH BUTTONS ///
+  document.getElementById('show_scoreboard').onclick=function(){
+     scoreboard.showScoreBoard();
+ };
+ document.getElementById('add_score').onclick=function(){
+     score = document.getElementById('lbl_score').innerHTML;
+     scoreboard.submitNewScoreDialog(score);
+ };
+
+ 
+ 
+function randomizeScore(){
+document.getElementById('lbl_score').innerHTML = String(Math.floor(Math.random()*1000));
+}
+ 
+  document.getElementById('randomize').onclick = randomizeScore;
+ 
+randomizeScore();
+    
+   
+
+
+
+$(init);
